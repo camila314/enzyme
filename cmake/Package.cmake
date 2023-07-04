9dbe784d18c5e11c19ff81999882cad8c47588f8
Decoder0007 <50407945+Decoder0007@users.noreply.github.com>
2023-07-04 01:15:14 -0700

    Actually fix whooper

--- cmake/Package.cmake
+++ cmake/Package.cmake
@@ -4,7 +4,7 @@ set(CMAKE_INSTALL_DEFAULT_COMPONENT_NAME "DEFAULT")
 function(enzyme_package target)
 	file(GLOB app_folder "${ENZYME_UNZIPPED_FOLDER}/Payload/*.app")
 	get_filename_component(app_name ${app_folder} NAME)
-	set(bin_folder ${Enzyme_BINARY_DIR}/enzyme_bin)
+	set(bin_folder enzyme_bin)
 
 	add_custom_target(EnzymePackage ALL
 		BYPRODUCTS ${CMAKE_BINARY_DIR}/${ENZYME_BINARY_NAME}.ipa
@@ -12,6 +12,6 @@ function(enzyme_package target)
 		COMMAND ${CMAKE_COMMAND} -E copy ${bin_folder}/${ENZYME_BINARY_NAME} ${bin_folder}/IPA/Payload/${app_name}/${ENZYME_BINARY_NAME}
 		COMMAND ${CMAKE_COMMAND} -E copy $<TARGET_FILE:${target}> ${bin_folder}/IPA/Payload/${app_name}/hook.dylib
 		COMMAND ${CMAKE_COMMAND} -E tar cfv "${CMAKE_BINARY_DIR}/${ENZYME_BINARY_NAME}.ipa" * --format=zip
-		WORKING_DIRECTORY ${bin_folder}/IPA
+		WORKING_DIRECTORY ${bin_folder}/../
 	)
 endfunction()
