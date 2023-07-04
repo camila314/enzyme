0f815b580fe0e22be528adae745838fa09dc578d
Decoder0007 <50407945+Decoder0007@users.noreply.github.com>
2023-07-04 01:57:55 -0700

    Super actually very yes fixed it

--- cmake/Package.cmake
+++ cmake/Package.cmake
@@ -4,14 +4,15 @@ set(CMAKE_INSTALL_DEFAULT_COMPONENT_NAME "DEFAULT")
 function(enzyme_package target)
 	file(GLOB app_folder "${ENZYME_UNZIPPED_FOLDER}/Payload/*.app")
 	get_filename_component(app_name ${app_folder} NAME)
-	set(bin_folder enzyme_bin)
+	set(bin_folder ${CMAKE_BINARY_DIR}/enzyme_bin)
 
 	add_custom_target(EnzymePackage ALL
 		BYPRODUCTS ${CMAKE_BINARY_DIR}/${ENZYME_BINARY_NAME}.ipa
+		MESSAGE "${bin_folder}"
 		COMMAND rsync -av ${ENZYME_UNZIPPED_FOLDER}/* ${bin_folder}/IPA
 		COMMAND ${CMAKE_COMMAND} -E copy ${bin_folder}/${ENZYME_BINARY_NAME} ${bin_folder}/IPA/Payload/${app_name}/${ENZYME_BINARY_NAME}
 		COMMAND ${CMAKE_COMMAND} -E copy $<TARGET_FILE:${target}> ${bin_folder}/IPA/Payload/${app_name}/hook.dylib
 		COMMAND ${CMAKE_COMMAND} -E tar cfv "${CMAKE_BINARY_DIR}/${ENZYME_BINARY_NAME}.ipa" * --format=zip
-		WORKING_DIRECTORY ${bin_folder}/../
+		WORKING_DIRECTORY ${bin_folder}/IPA
 	)
 endfunction()
